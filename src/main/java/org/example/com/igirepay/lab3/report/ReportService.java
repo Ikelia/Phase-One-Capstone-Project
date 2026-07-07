a16c103c30b97a68b6470be94cdf0b1c941df114
Kelia Mbonimpa <ikelia_mbonimpakelia@gmail.com>
2026-07-07 06:43:28 -0700

    Lab3: ReportService - CSV export daily summary statement Exercise 3.3

--- src/main/java/org/example/com/igirepay/lab3/report/ReportService.java
+++ src/main/java/org/example/com/igirepay/lab3/report/ReportService.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-public class ReportService {
+public class    ReportService {
 
     private final TransactionDAO transactionDAO = new TransactionDAO();
 
